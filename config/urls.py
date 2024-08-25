4bfb70f992dd141e49194c1c45a90b8e1f48d4d1
Mieda0310 <miedas@colabmix.co.jp>
2024-08-25 10:32:08 +0900

    管理画面を表示させる

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from mysite import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.index),
     path('blog/', include('blog.urls')),
 ]
