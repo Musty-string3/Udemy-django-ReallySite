4f55990ece466c5e7f73bf8682238620489581d7
Mieda0310 <miedas@colabmix.co.jp>
2024-08-29 23:17:28 +0900

    [update]コメント機能

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.contrib import messages
 from django.views import View
+from django.utils import timezone
+from datetime import timedelta
 
 from .models import Article, Comment
 from .forms import CommentForm
@@ -25,14 +27,29 @@ class ArticleIndexView(View):
 
 class ArticleDetailView(View):
     template_name = 'blog/article.html'
+    def days_ago_comment(self, comment_date):
+        delta = timezone.now() - comment_date
+        days_ago = delta.days
+
+        if delta < timedelta(minutes=1):
+            return 'たった今'
+        elif delta < timedelta(hours=1):
+            return f'{delta.seconds // 60}分前'
+        elif delta < timedelta(days=1):
+            return f'{delta.hours // 24}時間前'
+        else:
+            return f'{delta.days}日前'
 
     def get(self, request, pk, *args, **kwargs):
         article = Article.objects.get(pk=pk)
         comments = Comment.objects.filter(article=article)
+        comments_with_time = [(comment, self.days_ago_comment(comment.created_at)) for comment in comments]
+        # self.days_ago_comment()
+        print(comments_with_time)
 
         return render(request, self.template_name, {
             'article': article,
-            'comments': comments,
+            'comments_with_time': comments_with_time,
         })
 
     def post(self, request, pk, *args, **kwargs):
@@ -53,9 +70,10 @@ class ArticleDetailView(View):
             comment.save()
 
         comments = Comment.objects.filter(article=article)
+        comments_with_time = [(comment, self.days_ago_comment(comment.created_at)) for comment in comments]
 
         return render(request, self.template_name, {
             'article': article,
             'comment_form': comment_form,
-            'comments': comments,
+            'comments_with_time': comments_with_time,
         })
