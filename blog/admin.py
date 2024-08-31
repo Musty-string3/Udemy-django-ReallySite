61fd83615ac6a564d32188416e8af76a6a1ae23a
Mieda0310 <miedas@colabmix.co.jp>
2024-08-31 17:12:07 +0900

    タグモデルの修正

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Article, Comment, ArticleLike
+from .models import *
 
-admin.site.register(Article)
+class TagInline(admin.TabularInline):
+    model = Article.tags.through
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [TagInline]
+    exclude = ['tags', ]
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
 admin.site.register(ArticleLike)
+admin.site.register(ArticleTag)
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+class ArticleTag(models.Model):
+    slug = models.CharField(verbose_name='SLUG', unique=True, max_length=20, primary_key=True)
+    name = models.CharField(verbose_name='タグ名', unique=True, max_length=20)
+    created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'タグ'
+        db_table = 'article_tag'
+
+    def __str__(self):
+        return self.name
+
 class Article(models.Model):
     title = models.CharField(verbose_name='タイトル', default='タイトルです。', max_length=30, null=False, blank=False)
     text = models.TextField(verbose_name='テキスト', default='テキストです。', max_length=255, null=False, blank=False)
     author = models.CharField(verbose_name='作成者', max_length=50, null=False, blank=False)
+    tags = models.ManyToManyField(ArticleTag, verbose_name='タグ', related_name='articles')
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
 
@@ -18,7 +32,7 @@ class Article(models.Model):
 class Comment(models.Model):
     comment = models.TextField(verbose_name='コメント', max_length='500')
     user = models.ForeignKey(get_user_model(), verbose_name='投稿者', on_delete=models.CASCADE)
-    article = models.ForeignKey(Article, verbose_name='記事', on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, verbose_name='記事', on_delete=models.CASCADE, related_name='comments')
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
 
