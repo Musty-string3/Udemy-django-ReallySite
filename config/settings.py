2d659a9c52c51433ed932ea042d865eb705078d4
Mieda0310 <miedas@colabmix.co.jp>
2024-08-31 22:49:04 +0900

    staticフォルダの作成

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# ----- static 設定項目
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# ----- static 設定項目
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
