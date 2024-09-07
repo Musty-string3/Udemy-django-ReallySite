bc78ce85397d4bf20cd87e88457250d1b35b665d
Mieda0310 <miedas@colabmix.co.jp>
2024-09-07 18:09:59 +0900

    いいね機能の非同期処理

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('index/', views.ArticleIndexView.as_view(), name='index'),
+    path('new/', views.ArticleNewView.as_view(), name='new'),
     path('<int:pk>/', views.ArticleDetailView.as_view(), name='detail'),
     path('tags/<str:name>/', views.ArticleTagView.as_view(), name='tag_detail'),
+    path('<int:pk>/like/', views.ArticleLikeView.as_view(), name='like_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.contrib import messages
@@ -5,8 +7,12 @@ from django.views import View
 from django.db.models import Count
 
 from .models import Article, Comment, ArticleLike, ArticleTag
-from .forms import CommentForm
+from .forms import CommentForm, ArticleNewForm
 from common.myiste_def import *
+
+# 非同期処理
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.http import JsonResponse
 
 
 class ArticleIndexView(CustomLoginRequiredMixin, View):
@@ -26,6 +32,23 @@ class ArticleIndexView(CustomLoginRequiredMixin, View):
             'page_title': 'ブログ一覧画面',
             'paginator_articles': paginator,
             'page_number': page_number,
+        })
+
+
+class ArticleNewView(CustomLoginRequiredMixin, View):
+    template_name = 'mysite/new.html'
+
+    def get(self, request, *args, **kwargs):
+        article_new_form = ArticleNewForm()
+        
+
+        return render(request, self.template_name, {
+            
+        })
+
+    def post(self, request, *args, **kwargs):
+        return render(request, self.template_name, {
+            
         })
 
 
@@ -58,13 +81,14 @@ class ArticleDetailView(CustomLoginRequiredMixin, View):
 
         comment_form = CommentForm(request.POST)
 
-        if request.POST.get('like_count', None):
-            if request.POST.get('like_delete', None):
-                ArticleLike.objects.filter(user=request.user, article=article).delete()
-            elif article_like_exists(article, request.user):
-                ArticleLike.objects.create(user=request.user, article=article)
+        # 非同期処理を使わない、いいね機能
+        # if request.POST.get('like_count', None):
+        #     if request.POST.get('like_delete', None):
+        #         ArticleLike.objects.filter(user=request.user, article=article).delete()
+        #     elif article_like_exists(article, request.user):
+        #         ArticleLike.objects.create(user=request.user, article=article)
 
-        elif comment_form.is_valid():
+        if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.user = request.user
             comment.article = article
@@ -101,3 +125,44 @@ class ArticleTagView(CustomLoginRequiredMixin, View):
             'paginator_articles': paginator,
             'page_number': page_number,
         })
+
+class ArticleLikeView(CustomLoginRequiredMixin, View):
+
+    def get(self, request, pk, *args, **kwargs):
+        context = {
+            "message": "error",
+        }
+        return JsonResponse(context)
+
+    def post(self, request, pk, *args, **kwargs):
+        # javascriptから送られてきたbodyの中身を取得
+        data = json.loads(request.body)
+        # bodyの中のarticle_pkを取得
+        article_pk = data.get('article_pk')
+
+        context = {
+            "message": "error",
+        }
+        try:
+            article = Article.objects.get(pk=article_pk)
+            # article = Article.objects.get(pk=pk)でも可能
+        except Article.DoesNotExist:
+            messages.error(request, '存在しない記事です。')
+            return redirect('blog:index')
+
+        try:
+            like_exists = article_like_exists(article, request.user)
+
+            if like_exists:
+                ArticleLike.objects.create(user=request.user, article=article)
+                context["method"] = "create"
+            else:
+                ArticleLike.objects.filter(user=request.user, article=article).delete()
+                context["method"] = "delete"
+            context['message'] = 'success'
+            # いいねのカウント数を集計
+            context["like_count"] = article.article_like.count()
+        except:
+            pass
+
+        return JsonResponse(context)
