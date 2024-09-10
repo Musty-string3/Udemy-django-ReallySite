5156aa1a6090ce0be676d481563b794fa13bd60f
Mieda0310 <miedas@colabmix.co.jp>
2024-09-10 23:17:32 +0900

    プロフィール情報のiconをdefaultを設定

--- mysite/models/profile_models.py
+++ mysite/models/profile_models.py
@@ -17,7 +17,7 @@ class Profile(models.Model):
     prefecture = models.CharField(verbose_name='都道府県',default='', max_length=8)
     city = models.CharField(verbose_name='市区町村', default='', max_length=100)
     address = models.CharField(verbose_name='住所', default='', max_length=200)
-    image = models.ImageField(verbose_name='プロフィール画像', upload_to=upload_image_to, default='static/mysite/IMG_1131.jpg')
+    image = models.ImageField(verbose_name='プロフィール画像', upload_to=upload_image_to, default='images/default.png')
 
     class Meta:
         verbose_name_plural = 'プロフィール'
