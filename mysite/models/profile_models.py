543508c805ec602b592b50d8f7b1728874720416
Mieda0310 <miedas@colabmix.co.jp>
2024-09-11 15:44:41 +0900

    都道府県の追加

--- mysite/models/profile_models.py
+++ mysite/models/profile_models.py
@@ -9,12 +9,63 @@ def upload_image_to(instance, filename):
     user_id = str(instance.user.id)
     return os.path.join('images', user_id, filename)
 
+Prefecture = (
+    (0, '選択なし'),
+    (1, '北海道'),
+    (2, '青森'),
+    (3, '岩手'),
+    (4, '宮城'),
+    (5, '秋田'),
+    (6, '山形'),
+    (7, '福島'),
+    (8, '茨城'),
+    (9, '栃木'),
+    (10, '群馬'),
+    (11, '埼玉'),
+    (12, '千葉'),
+    (13, '東京'),
+    (14, '神奈川'),
+    (15, '新潟'),
+    (16, '富山'),
+    (17, '石川'),
+    (18, '福井'),
+    (19, '山梨'),
+    (20, '長野'),
+    (21, '岐阜'),
+    (22, '静岡'),
+    (23, '愛知'),
+    (24, '三重'),
+    (25, '滋賀'),
+    (26, '京都'),
+    (27, '大阪'),
+    (28, '兵庫'),
+    (29, '奈良'),
+    (30, '和歌山'),
+    (31, '鳥取'),
+    (32, '島根'),
+    (33, '岡山'),
+    (34, '広島'),
+    (35, '山口'),
+    (36, '徳島'),
+    (37, '香川'),
+    (38, '愛媛'),
+    (39, '高知'),
+    (40, '福岡'),
+    (41, '佐賀'),
+    (42, '長崎'),
+    (43, '熊本'),
+    (44, '大分'),
+    (45, '宮崎'),
+    (46, '鹿児島'),
+    (47, '沖縄')
+)
+
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name='ユーザー', unique=True, on_delete=models.CASCADE, primary_key=True)
     username = models.CharField(verbose_name='ユーザー名', default='匿名ユーザー', max_length=50)
     zipcode = models.CharField(verbose_name='郵便番号', default='', max_length=8)
-    prefecture = models.CharField(verbose_name='都道府県',default='', max_length=8)
+    prefecture = models.CharField(verbose_name='都道府県',choices=Prefecture, default=0, max_length=8)
     city = models.CharField(verbose_name='市区町村', default='', max_length=100)
     address = models.CharField(verbose_name='住所', default='', max_length=200)
     image = models.ImageField(verbose_name='プロフィール画像', upload_to=upload_image_to, default='images/default.png')
