925d0ea49dc721ad21c2e5579043d77eff01f506
Mieda0310 <miedas@colabmix.co.jp>
2024-09-22 22:23:03 +0900

    全てのビューで共通して利用できるコンテキストプロセッサを作成

---
+++ config/context_processors.py
@@ -0,0 +1,8 @@
+from blog.models import Notification
+
+# SettingsのTEMPLATESの中に記述することでどこでも通知のカウント数を得られる
+def notification_count(request):
+    count = 0
+    if request.user.is_authenticated:
+        count = Notification.objects.filter(user=request.user).count()
+    return {'notification_count': count}
