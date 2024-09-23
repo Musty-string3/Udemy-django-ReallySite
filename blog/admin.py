f5184a1bcfd96ec0688531ec0c34a9472e42ecb7
Mieda0310 <miedas@colabmix.co.jp>
2024-09-23 20:12:15 +0900

    DM機能のモデル作成と管理画面のレイアウト修正

--- blog/admin.py
+++ blog/admin.py
@@ -1,20 +1,65 @@
 from django.contrib import admin
 from .models import *
 
+
 class TagInline(admin.TabularInline):
     model = Article.tags.through
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
 class ArticleAdmin(admin.ModelAdmin):
-    inlines = [TagInline]
+    list_display = ("id", "title", "text", "author", "is_public", "sell_flag", "price", "created_at", "updated_at")
+    inlines = [TagInline, ImageInline]
     exclude = ['tags', ]
 
+class ArticleTagAdmin(admin.ModelAdmin):
+    list_display = ("slug", "name", "created_at")
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ("id", "image", "article", "created_at")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "comment", "user", "article", "created_at")
+
+class ArticleLikeAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "article", "created_at")
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "article", "price", "charge_type", "order_status", "created_at")
+
+class UserItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "article", "charge_type", "created_at")
+
+class ViewCountAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "article", "created_at")
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("id", "follower", "followed", "created_at")
+
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ("id", "user1", "user2", "created_at")
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "conversation", "sender", "text", "is_read", "created_at")
+
+class MessageAttachmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "message", "file_url", "file_type", "created_at")
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "sender", "action_type", "article", "comment", "like", "follow", "is_read", "created_at")
+
+
 admin.site.register(Article, ArticleAdmin)
-admin.site.register(Comment)
-admin.site.register(ArticleLike)
-admin.site.register(ArticleTag)
-admin.site.register(Order)
-admin.site.register(UserItem)
-admin.site.register(ViewCount)
-admin.site.register(Follow)
-admin.site.register(Image)
-admin.site.register(Notification)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(ArticleLike, ArticleLikeAdmin)
+admin.site.register(ArticleTag, ArticleTagAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(UserItem, UserItemAdmin)
+admin.site.register(ViewCount, ViewCountAdmin)
+admin.site.register(Follow, FollowAdmin)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Notification, NotificationAdmin)
+admin.site.register(Conversation, ConversationAdmin)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(MessageAttachment, MessageAttachmentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,11 @@ def upload_article_image_to(instance, filename):
     return os.path.join('article', 'images', user_id, article_id, filename)
 
 
+def upload_dm_image_to(instance, filename):
+    user_id = 'UserID:' + str(instance.message.sender.id)
+    return os.path.join('dm', 'images', user_id, filename)
+
+
 class ArticleTag(models.Model):
     slug = models.CharField(verbose_name='SLUG', unique=True, max_length=20, primary_key=True)
     name = models.CharField(verbose_name='タグ名', unique=True, max_length=20, db_index=True)
@@ -23,7 +28,7 @@ class ArticleTag(models.Model):
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
 
     class Meta:
-        verbose_name_plural = 'タグ'
+        verbose_name_plural = '01-03.タグ'
         db_table = 'article_tag'
 
     def __str__(self):
@@ -41,7 +46,7 @@ class Article(models.Model):
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
 
     class Meta:
-        verbose_name_plural = '記事'
+        verbose_name_plural = '01-01.記事'
         db_table = 'article'
 
     def __str__(self):
@@ -57,7 +62,7 @@ class Image(models.Model):
 
 
     class Meta:
-        verbose_name_plural = '投稿画像'
+        verbose_name_plural = '01-02.投稿画像'
         db_table = 'article_image'
 
 
@@ -70,7 +75,7 @@ class Comment(models.Model):
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
 
     class Meta:
-        verbose_name_plural = 'コメント'
+        verbose_name_plural = '02-02.コメント'
         db_table = 'comment'
 
     def __str__(self):
@@ -84,7 +89,7 @@ class ArticleLike(models.Model):
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
 
     class Meta:
-        verbose_name_plural = 'いいね'
+        verbose_name_plural = '02-01.いいね'
         db_table = 'article_like'
 
     def __str__(self):
@@ -109,7 +114,7 @@ class Order(models.Model):
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
 
     class Meta:
-        verbose_name_plural = 'ユーザー注文履歴'
+        verbose_name_plural = '12-01.ユーザー注文履歴'
         db_table = 'order'
 
 
@@ -121,7 +126,7 @@ class UserItem(models.Model):
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
 
     class Meta():
-        verbose_name_plural = 'ユーザー購入商品'
+        verbose_name_plural = '04-02.ユーザー購入商品'
         unique_together = ('user', 'article')
         db_table = 'user_item'
 
@@ -132,7 +137,7 @@ class ViewCount(models.Model):
     updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
 
     class Meta:
-        verbose_name_plural = '記事の閲覧カウント'
+        verbose_name_plural = '02-03.記事の閲覧カウント'
         db_table = 'view_count'
         indexes = [
             models.Index(fields=['article', 'created_at']),
@@ -153,7 +158,7 @@ class Follow(models.Model):
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = 'フォロー'
+        verbose_name_plural = '03-01.フォロー'
         db_table = 'user_follow'
         unique_together = ('follower', 'followed')
 
@@ -167,19 +172,67 @@ ACTION_TYPE = (
     ('purchase', 'Purchase'),
 )
 
+
+class Conversation(models.Model):
+    user1 = models.ForeignKey(get_user_model(), verbose_name="ユーザー1", on_delete=models.CASCADE, related_name="conversation_user1")
+    user2 = models.ForeignKey(get_user_model(), verbose_name="ユーザー2", on_delete=models.CASCADE, related_name="conversation_user2")
+    created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
+
+    class Meta():
+        verbose_name_plural = '06-01.DMルーム'
+        db_table = 'conversation_room'
+        constraints = [
+            models.UniqueConstraint(fields=['user1', 'user2'], name='unique_conversation')
+        ]
+
+    def __str__(self):
+        return f'Conversation: {self.user1} - {self.user2}'
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, verbose_name="DMルーム", on_delete=models.CASCADE, related_name="messages")
+    sender = models.ForeignKey(get_user_model(), verbose_name="送信したユーザー", on_delete=models.CASCADE, related_name="messages")
+    text = models.TextField(verbose_name="メッセージ内容")
+    is_read = models.BooleanField(verbose_name="既読", default=False)
+    created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
+
+    class Meta():
+        verbose_name_plural = '06-02.DMメッセージ内容'
+        db_table = 'dm_message'
+
+    def __str__(self):
+        return f'Message: {self.sender}'
+
+
+class MessageAttachment(models.Model):
+    message = models.ForeignKey(Message, verbose_name="メッセージ", on_delete=models.CASCADE)
+    file_url = models.FileField(verbose_name="ファイル保存先", upload_to=upload_dm_image_to)
+    file_type = models.SmallIntegerField(verbose_name="ファイルタイプ")
+    created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
+
+
+    class Meta():
+        verbose_name_plural = '06-03.DMメッセージファイル'
+        db_table = 'dm_message_attachment'
+
+    def __str__(self):
+        return f'MessageAttachment: {self.id}'
+
+
 class Notification(models.Model):
     user = models.ForeignKey(get_user_model(), verbose_name="通知を受け取るユーザー", on_delete=models.CASCADE, related_name="notifications", db_index=True)
     sender = models.ForeignKey(get_user_model(), verbose_name="通知したユーザー", on_delete=models.SET_NULL, blank=True, null=True, related_name="notified", db_index=True)
     action_type = models.CharField(verbose_name="通知タイプ", choices=ACTION_TYPE, max_length=50, db_index=True)
     article = models.ForeignKey(Article, verbose_name="記事", on_delete=models.CASCADE, null=True, blank=True, related_name='notifications')
     comment = models.ForeignKey(Comment, verbose_name="コメント", on_delete=models.CASCADE, null=True, blank=True, related_name='notifications')
-    like = models.ForeignKey(ArticleLike, verbose_name="記事", on_delete=models.CASCADE, null=True, blank=True, related_name='notifications')
+    like = models.ForeignKey(ArticleLike, verbose_name="いいね", on_delete=models.CASCADE, null=True, blank=True, related_name='notifications')
     follow = models.ForeignKey(Follow, verbose_name="フォロー", on_delete=models.CASCADE, null=True, blank=True, related_name='notifications')
+    # dm = models.ForeignKey(Follow, verbose_name="DM", on_delete=models.CASCADE, null=True, blank=True, related_name='notifications')
     is_read = models.BooleanField(verbose_name='既読', default=0, db_index=True, blank=True)
     created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
 
     class Meta():
-        verbose_name_plural = '通知'
+        verbose_name_plural = '05-01.通知'
         db_table = 'notification'
 
     def __str__(self):
